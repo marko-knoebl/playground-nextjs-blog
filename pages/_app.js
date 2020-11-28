763c405382ccc16c1c3d13f2ad6fad64593efac6
Marko Knöbl <marko.kn@gmail.com>
2020-11-28 21:11:48 +0100

    first step: login with redirect attempt

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/global.css'
+import { Auth0Provider } from "@auth0/auth0-react";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain="dev-5qv6e2f9.eu.auth0.com"
+      clientId="qOWLgvAq44ctOsTT9cuo8U1QhsQE99Tv"
+      redirectUri="https://playground-nextjs-blog.vercel.app/"
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Home({ allPostsData }) {
+  const auth = useAuth0();
+
+  if (auth.isLoading) {
+    return <button onClick={auth.loginWithRedirect}>Log in</button>;
+  } else if (auth.error) {
+    return <div>error...</div>;
+  } else if (!auth.isAuthenticated) {
+    return <button onClick={auth.loginWithRedirect}>Log in</button>;
+  } else {
+    return (
+      <div>
+        main content
+        <button onClick={auth.logout}>log out</button>
+      </div>
+    );
+  }
   return (
     <Layout home>
       <Head>
@@ -14,7 +31,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
+          (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
@@ -35,14 +52,14 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
